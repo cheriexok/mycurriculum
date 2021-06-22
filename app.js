56d5414b95a6556cfcac7be1d4e277cc28c58cfd
Caroline Nathaly <carolinenathaly5@gmail.com>
2021-06-22 15:45:32 -0300

    produção

---
+++ app.js
@@ -0,0 +1,50 @@
+var exibirTeste1 = function() {
+    divTest1 = document.querySelector('#teste1');
+    divTest2 = document.querySelector('#teste2');
+    divTest3 = document.querySelector('#teste3');
+    divTest4 = document.querySelector('#teste4');
+
+    divTest1.style.display = "block";
+    divTest2.style.display = "none";
+    divTest3.style.display = "none";
+    divTest4.style.display = "none";
+
+}
+
+var exibirTeste2 = function() {
+    divTest1 = document.querySelector('#teste1');
+    divTest2 = document.querySelector('#teste2');
+    divTest3 = document.querySelector('#teste3');
+    divTest4 = document.querySelector('#teste4');
+
+    divTest1.style.display = "none";
+    divTest2.style.display = "block";
+    divTest3.style.display = "none";
+    divTest4.style.display = "none";
+
+}
+var exibirTeste3 = function() {
+    divTest1 = document.querySelector('#teste1');
+    divTest2 = document.querySelector('#teste2');
+    divTest3 = document.querySelector('#teste3');
+    divTest4 = document.querySelector('#teste4');
+
+    divTest1.style.display = "none";
+    divTest2.style.display = "none";
+    divTest3.style.display = "block";
+    divTest4.style.display = "none";
+
+}
+
+var exibirTeste4 = function() {
+    divTest1 = document.querySelector('#teste1');
+    divTest2 = document.querySelector('#teste2');
+    divTest3 = document.querySelector('#teste3');
+    divTest4 = document.querySelector('#teste4');
+
+    divTest1.style.display = "none";
+    divTest2.style.display = "none";
+    divTest3.style.display = "none";
+    divTest4.style.display = "block";
+
+}
